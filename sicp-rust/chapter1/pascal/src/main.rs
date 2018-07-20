31adb07eb336841b00dfd4494b82b9d40ebc8620
Arul Madhavan <arul.madhavan@arulmadhavan-MBP-1433.local>
2018-07-20 07:01:34 -0700

    Pascal with test

--- sicp-rust/chapter1/pascal/src/main.rs
+++ sicp-rust/chapter1/pascal/src/main.rs
@@ -1,11 +1,33 @@
-fn pascal_rec(n:u64, level:u64, previous:vec<u64>) -> vec<u64> = {
+fn pascal_rec(n: usize, level: usize, previous: Vec<usize>) -> Vec<usize> {
     if level == n {
         previous
     } else {
-        previous.iter().fold(Vec::new(),|acc, elem| )
+        let mut next = Vec::with_capacity(level);
+        previous.into_iter().fold(0usize, |p, elem| {
+            next.push(p + elem);
+            elem
+        });
+        next.push(1);
+        pascal_rec(n, level + 1, next)
     }
+}
+
+fn pascal(n: usize) -> Vec<usize> {
+    pascal_rec(n, 1, vec![1])
 }
 
 fn main() {
     println!("Hello, world!");
+    println!("Pascal {:?}", pascal(5));
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(pascal(5), vec![1, 4, 6, 4, 1]);
+    }
 }
