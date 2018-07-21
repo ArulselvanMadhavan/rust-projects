ac66b2391a3924814c02e625cc055075b637d51c
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-07-21 10:25:16 -0700

    Real fast expt

--- sicp-rust/chapter1/fast-expt/src/main.rs
+++ sicp-rust/chapter1/fast-expt/src/main.rs
@@ -1,49 +1,57 @@
 extern crate num_integer;
 use num_integer::Integer;
-use std::cmp::Ordering;
 
 fn square(res: usize) -> usize {
     res * res
 }
 
-fn fast_expt_rec(value: usize, n: usize) -> usize {
+fn fast_expt_rec_process(value: usize, n: usize) -> usize {
     if n == 0 {
         1
     } else {
         match n.is_even() {
-            true => square(fast_expt_rec(value, n / 2)),
-            false => value * fast_expt_rec(value, n - 1),
+            true => square(fast_expt_rec_process(value, n / 2)),
+            false => value * fast_expt_rec_process(value, n - 1),
+        }
+    }
+}
+// a b^n - loop invariant
+#[allow(dead_code)]
+fn fast_expt_rec_procedure(a: usize, b: usize, n: usize) -> usize {
+    if n == 1 {
+        a * b
+    } else {
+        match n.is_even() {
+            true => fast_expt_rec_procedure(a, b * b, n / 2),
+            false => fast_expt_rec_procedure(a * b, b, n - 1),
         }
     }
 }
 
-// Takes more steps to converge.
-fn fast_expt_iter(value: usize, n: usize, mut counter: usize, mut result: usize) -> usize {
-    loop {
-        counter = counter * 2;
-        match counter.cmp(&n) {
-            Ordering::Less => result = square(result),
-            Ordering::Equal => {
-                result = square(result);
-                break;
+fn fast_expt_iter(mut a: usize, mut b: usize, mut n: usize) -> usize {
+    while n != 1 {
+        match n.is_even() {
+            true => {
+                n = n / 2;
+                b = b * b;
             }
-            Ordering::Greater => result = result * fast_expt(value, counter - n),
+            false => {
+                n = n - 1;
+                a = a * b;
+            }
         }
     }
-    result
+    a * b
 }
 
 fn fast_expt(value: usize, n: usize) -> usize {
-    match n {
-        0 => 1,
-        1 => value,
-        _ => fast_expt_iter(value, n, 1, value),
-    }
+    // fast_expt_rec_procedure(1, value, n)
+    fast_expt_iter(1, value, n)
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("Result Rec:{}", fast_expt_rec(2, 32));
+    println!("Result Rec:{}", fast_expt_rec_process(2, 32));
     println!("Result Iter:{}", fast_expt(2, 32));
 }
 
