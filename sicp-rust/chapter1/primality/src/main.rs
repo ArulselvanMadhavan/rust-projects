12256e4db0a2cc2709a9e6d637f4eca8e90eaa48
Arul Madhavan <arulselvan1234@gmail.com>
2018-07-26 20:30:35 -0700

    Cool Prime

--- sicp-rust/chapter1/primality/src/main.rs
+++ sicp-rust/chapter1/primality/src/main.rs
@@ -2,32 +2,57 @@ extern crate num_integer;
 extern crate rand;
 use num_integer::Integer;
 use rand::prelude::*;
+use std::time::Instant;
 
+#[allow(dead_code)]
 fn square(n: usize) -> usize {
     n * n
 }
 
-fn exp_mod(a: usize, b: usize, n: usize) -> usize {
+fn exp_mod(mut a: usize, mut b: usize, mut n: usize, m: usize) -> usize {
+    while n != 1 {
+        match n.is_even() {
+            true => {
+                n = n / 2;
+                b = (b * b) % m; // a * (b^2 mod m)
+            }
+            false => {
+                n = n - 1;
+                a = (a * b) % m;
+            }
+        }
+    }
+    (a * b) % m
+}
+
+#[allow(dead_code)]
+fn exp_mod_rec(a: usize, b: usize, n: usize) -> usize {
     if b == 0 {
         1
     } else {
         match n.is_even() {
-            true => square(exp_mod(a, b / 2, n)) % n,
-            false => (a * exp_mod(a, b - 1, n)) % n,
+            true => square(exp_mod_rec(a, b / 2, n)) % n,
+            false => (a * exp_mod_rec(a, b - 1, n)) % n,
         }
     }
 }
 
-fn fast_prime_helper(mut rng: ThreadRng, n: usize, times: usize) -> bool {
-    if times == 0 {
-        true
-    } else {
+// Hate the fact that Rust won't support tail recursion.
+fn fast_prime_helper(mut rng: ThreadRng, n: usize, mut times: usize) -> bool {
+    let mut result = true;
+    while times != 0 {
         let a = rng.gen_range(2, n);
-        match exp_mod(a, n, n) == a {
-            true => fast_prime_helper(rng, n, times - 1),
-            false => false, // It is NOT PRIME
+        match exp_mod(1, a, n, n) == a {
+            true => {
+                times = times - 1;
+            }
+            false => {
+                result = false;
+                times = 0; // Exit the loop
+            }
         }
     }
+    result
 }
 
 fn fast_prime(n: usize, times: usize) -> bool {
@@ -35,7 +60,17 @@ fn fast_prime(n: usize, times: usize) -> bool {
 }
 
 fn main() {
-    println!("Hello, world!{}", fast_prime(3, 5));
+    let mut res: Vec<_> = (100000..500000)
+        .map(|x| {
+            let start: Instant = Instant::now();
+            let p = fast_prime(x, 10);
+            (x, Instant::now().duration_since(start), p)
+        })
+        .filter(|x| x.2)
+        .collect();
+
+    res.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+    res.iter().take(10).for_each(|x| println!("{:?}", x));
 }
 
 #[cfg(test)]
@@ -43,6 +78,8 @@ mod tests {
     use super::*;
     #[test]
     fn basic_tests() {
-        assert_eq!(fast_prime(3), true);
+        assert_eq!(fast_prime(3, 5), true);
+        assert_eq!(exp_mod_rec(2, 5, 5), exp_mod(1, 2, 5, 5));
+        assert_eq!(exp_mod_rec(3, 7, 7), exp_mod(1, 3, 7, 7));
     }
 }
